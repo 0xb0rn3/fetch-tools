0712ce30319180fc48e070de2bc8074a29ce6241
b0urn3 <154826956+q4n0@users.noreply.github.com>
2024-11-06 16:59:30 +0300

    Update dragon_fetch.c

--- src/dragon_fetch.c
+++ src/dragon_fetch.c
@@ -1,6 +1,6 @@
 #include "common/fetch.h"
 
-const char* DRAGON_ART[] = {
+const char* ANIME_ART[] = {
     "⠀⠀⠀⠀⢀⡠⠤⠔⢲⢶⡖⠒⠤⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
     "⠀⠀⣠⡚⠁⢀⠀⠀⢄⢻⣿⠀⠀⠀⡙⣷⢤⡀⠀⠀⠀⠀⠀⠀",
     "⠀⡜⢱⣇⠀⣧⢣⡀⠀⡀⢻⡇⠀⡄⢰⣿⣷⡌⣢⡀⠀⠀⠀⠀",
@@ -18,29 +18,43 @@ const char* DRAGON_ART[] = {
     "⠀⠀⠘⢼⣿⠀⢠⣤⣀⠉⣹⡿⠀⠁⠀⡸⠀⠀⠀⠈⣿⡇⠀⠀"
 };
 
+void print_color_blocks() {
+    printf("\n");
+    // Print color blocks
+    for(int i = 0; i < 8; i++) {
+        printf("\033[4%dm   ", i);
+    }
+    printf(RESET "\n");
+    for(int i = 0; i < 8; i++) {
+        printf("\033[10%dm   ", i);
+    }
+    printf(RESET "\n\n");
+}
+
 int main() {
     SystemInfo info;
     get_system_info(&info);
     
     printf("\n");
     int info_line = 0;
-    for (int i = 0; i < sizeof(DRAGON_ART) / sizeof(DRAGON_ART[0]); i++) {
-        printf("%s%s%s", RED, DRAGON_ART[i], RESET);
+    
+    for (int i = 0; i < sizeof(ANIME_ART) / sizeof(ANIME_ART[0]); i++) {
+        printf("%s%s%s", CYAN, ANIME_ART[i], RESET);
         
         switch(info_line) {
             case 0:
-                printf("  %s%s%s@%s%s%s", BOLD, info.username, BLUE, info.hostname, RESET, RESET);
+                printf("  %s%s%s@%s%s%s", BOLD, info.username, CYAN, info.hostname, RESET, RESET);
                 break;
             case 1:
                 printf("  %s%s%s", BOLD, "OS: ", RESET);
                 printf("%s", info.os_name);
                 break;
             case 2:
-                printf("  %s%s%s", BOLD, "Kernel: ", RESET);
+                printf("  %s%s%s", BOLD, "KERNEL: ", RESET);
                 printf("%s", info.kernel);
                 break;
             case 3:
-                printf("  %s%s%s", BOLD, "Shell: ", RESET);
+                printf("  %s%s%s", BOLD, "SHELL: ", RESET);
                 printf("%s", info.shell);
                 break;
             case 4:
@@ -48,21 +62,22 @@ int main() {
                 printf("%s", info.cpu_info);
                 break;
             case 5:
-                printf("  %s%s%s", BOLD, "Memory: ", RESET);
+                printf("  %s%s%s", BOLD, "MEMORY: ", RESET);
                 printf("%s", info.memory);
                 break;
             case 6:
-                printf("  %s%s%s", BOLD, "Disk: ", RESET);
+                printf("  %s%s%s", BOLD, "DISK: ", RESET);
                 printf("%s", info.disk_space);
                 break;
             case 7:
-                printf("  %s%s%s", BOLD, "Uptime: ", RESET);
+                printf("  %s%s%s", BOLD, "UPTIME: ", RESET);
                 printf("%s", info.uptime);
                 break;
         }
         printf("\n");
         info_line++;
     }
-    printf("\n");
+    
+    print_color_blocks();
     return 0;
 }
